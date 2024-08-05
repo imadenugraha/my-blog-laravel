7c42867865d3cfc97a27169f2b1cb7cceefaf6c1
imadenugraha <imadenugraha@gmail.com>
2024-08-05 14:33:12 +0000

    refactor: move posts and find function to Post.php in models

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
+use App\Models\Post;
+
 
 Route::redirect('/', '/home', 301);
 
@@ -14,56 +16,14 @@ Route::get('/home', function () {
 Route::get('/posts', function() {
     return view('posts', [
         'routeTitle' => 'Posts',
-        'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul 1',
-                'author' => 'Made',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi consequuntur voluptates cumque. 
-                Neque magni itaque hic temporibus tempora in, quo, mollitia amet voluptatibus commodi minus quod? Autem veniam ducimus maxime.',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul 2',
-                'author' => 'Made',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Neque libero, iusto, exercitationem animi quidem eum atque repellat cumque molestiae saepe nam. Doloremque rerum quia error cum. 
-                Debitis, laboriosam libero. Iusto.',
-            ],
-        ]
+        'posts' => Post::all(),
     ]);
 });
 
 Route::get('/posts/{slug}', function($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul 1',
-            'author' => 'Made',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi consequuntur voluptates cumque. 
-            Neque magni itaque hic temporibus tempora in, quo, mollitia amet voluptatibus commodi minus quod? Autem veniam ducimus maxime.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul 2',
-            'author' => 'Made',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Neque libero, iusto, exercitationem animi quidem eum atque repellat cumque molestiae saepe nam. Doloremque rerum quia error cum. 
-            Debitis, laboriosam libero. Iusto.',
-        ],
-    ];
-
-    $post = Arr::first($posts, function($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
-
     return view('post', [
         'routeTitle' => 'Single Post',
-        'post' => $post,
+        'post' => Post::find($slug),
     ]);
 });
 
