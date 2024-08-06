a9019f1c73761b783548b2ed2d306b65791185c6
imadenugraha <imadenugraha@gmail.com>
2024-08-06 11:25:56 +0700

    feat: model for Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,42 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul 1',
-                'author' => 'Made',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi consequuntur voluptates cumque. 
-                Neque magni itaque hic temporibus tempora in, quo, mollitia amet voluptatibus commodi minus quod? Autem veniam ducimus maxime.',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul 2',
-                'author' => 'Made',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Neque libero, iusto, exercitationem animi quidem eum atque repellat cumque molestiae saepe nam. Doloremque rerum quia error cum. 
-                Debitis, laboriosam libero. Iusto.',
-            ],
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-    
-        if(!$post) {
-            abort(404);
-        } else {
-            return $post;
-        }
-    }
+    protected $fillable = [
+        'title',
+        'author',
+        'slug',
+        'body',
+    ];
 }
